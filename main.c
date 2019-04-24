f4bb12b60b484250d498455b3e22ff87cd42ad36
nicolaschillemi <nicolaschillemi@hotmail.com>
2019-04-24 14:43:13 -0300

    Ejercicio uno de la guia de Programacion Estructurada 1

---
+++ main.c
@@ -0,0 +1,21 @@
+/* Ingresar un numero y visualizarlo en pantalla */
+
+#include <stdio.h>
+
+int main()
+
+{
+
+    int numero;
+
+
+    printf("Ingrese un numero: ");
+
+    scanf("%i",&numero);
+
+    printf("El numero ingrsado es: %i",numero);
+
+    return 0;
+
+
+}
